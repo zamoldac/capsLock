ac8cd5c52cabfd7e9ebbe7bba997a299272c0305
Alex <alexandru.ghita@uipath.com>
2026-05-28 18:08:00 +0300

    adding second test for form quote flow

--- pages/mainPage.ts
+++ pages/mainPage.ts
@@ -3,13 +3,27 @@ import * as testData from '../testData/testData.json';
 
 export class MainPage {
   readonly page: Page;
+  readonly enterZipCodeInput: Locator;
+  readonly submitButtonStep1: Locator;
 
 
   constructor(page: Page) {
     this.page = page;
+    this.enterZipCodeInput = page.locator('[placeholder="Enter ZIP Code"]').nth(0);
+    this.submitButtonStep1 = page.locator('[data-tracking="btn-step-1"]').nth(0);
    }
 
     async goToMainPage() {
     await this.page.goto('https://test-qa.capslock.global/');
    }
+
+    async enterZipCode(zipCode: string) {
+    await this.enterZipCodeInput.scrollIntoViewIfNeeded();
+    await this.enterZipCodeInput.fill(zipCode);
+   }
+
+    async clickSubmitButtonStep1() {
+    await this.submitButtonStep1.click();
+   }
+
  }
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -7,3 +7,11 @@ test('Verify that the main page is loaded', async ({ page }) => {
   await mainPage.goToMainPage();
   await expect(page).toHaveTitle(testData.validDataSet.mainPageTitle);
 });
+
+test('Ask for a cost quote scenario - Valid e2e flow with form steps verification', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.goToMainPage();
+  await mainPage.enterZipCode(testData.validDataSet.zipNumberValid);
+  await mainPage.clickSubmitButtonStep1();
+ // at this point, should add step no verification
+});
