cc12b7a557d94d68ae05fb09718f779b300fb30e
Alex <alexandru.ghita@uipath.com>
2026-06-05 17:54:46 +0300

    adding more tests

--- pages/mainPage.ts
+++ pages/mainPage.ts
@@ -5,25 +5,94 @@ export class MainPage {
   readonly page: Page;
   readonly enterZipCodeInput: Locator;
   readonly submitButtonStep1: Locator;
+  readonly independeceOptionStep2: Locator;
+  readonly safetyOptionStep2: Locator;
+  readonly therapyOptionStep2: Locator;
+  readonly otherOptionsStep2: Locator;
+  readonly nextButtonStep2: Locator;
+  readonly ownedOptionStep3: Locator;
+  readonly rentalOptionStep3: Locator;
+  readonly mobileOoptionStep3: Locator;
+  readonly nextButtonStep3: Locator;
+  readonly nameFieldStep4: Locator;
+  readonly emailFieldStep4: Locator;
+  readonly goToEstimateStep4: Locator;
+  readonly phoneNumberFieldStep5: Locator;
+  readonly submitYourRequestButtonStep5: Locator;
+  readonly stepCounter: Locator;
 
 
   constructor(page: Page) {
     this.page = page;
     this.enterZipCodeInput = page.locator('[placeholder="Enter ZIP Code"]').nth(0);
     this.submitButtonStep1 = page.locator('[data-tracking="btn-step-1"]').nth(0);
+    this.independeceOptionStep2 = page.locator('[for="why-interested-independence-1"]').nth(0);
+    this.safetyOptionStep2 = page.locator('[for="why-interested-safety-1"]').nth(0);
+    this.therapyOptionStep2 = page.locator('[for="why-interested-therapy-1"]').nth(0);
+    this.otherOptionsStep2 = page.locator('[for="why-interested-other-1"]').nth(0);
+    this.nextButtonStep2 = page.locator('[data-tracking="btn-step-2"]').nth(0);
+    this.ownedOptionStep3 = page.locator('[for="homeowner-owned-1"]').nth(0);
+    this.rentalOptionStep3 = page.locator('[for="homeowner-rental-1"]').nth(0);
+    this.mobileOoptionStep3 = page.locator('[for="homeowner-mobile-1"]').nth(0);
+    this.nextButtonStep3 = page.locator('[data-tracking="btn-step-3"]').nth(0);
+    this.nameFieldStep4 = page.locator('[placeholder="Enter Your Name"]').nth(0);
+    this.emailFieldStep4 = page.locator('[placeholder="Enter Your Email"]').nth(0);
+    this.goToEstimateStep4 = page.locator('[data-tracking="btn-step-4"]').nth(0);
+    this.phoneNumberFieldStep5 = page.locator('[placeholder="(XXX)XXX-XXXX"]').nth(0);
+    this.submitYourRequestButtonStep5 = page.locator('[data-tracking="btn-step-5"]').nth(0);
+    this.stepCounter = page.locator('[data-current-step="1"]').nth(0);
    }
 
-    async goToMainPage() {
+    async goToMainPage(){
     await this.page.goto('https://test-qa.capslock.global/');
    }
-
-    async enterZipCode(zipCode: string) {
+    async verifyFormCurrentStepNumber(expectedStepCounter: string){
+    const stepCounterText = await this.stepCounter.textContent();
+    //console.log(stepCounterText);
+    await expect(stepCounterText).toBe(expectedStepCounter);
+   }
+    async enterZipCode(zipCode: string){
     await this.enterZipCodeInput.scrollIntoViewIfNeeded();
     await this.enterZipCodeInput.fill(zipCode);
    }
-
-    async clickSubmitButtonStep1() {
+    async enterNameStep4(name: string){
+    await this.nameFieldStep4.scrollIntoViewIfNeeded();
+    await this.nameFieldStep4.fill(name);
+   }
+    async enterEmailStep4(email: string){
+    await this.emailFieldStep4.scrollIntoViewIfNeeded();
+    await this.emailFieldStep4.fill(email);
+   }
+    async enterPhoneNumberStep5(phoneNumber: string){
+    await this.phoneNumberFieldStep5.scrollIntoViewIfNeeded();
+    await this.phoneNumberFieldStep5.fill(phoneNumber);
+   }
+    async clickSubmitButtonStep1(){
     await this.submitButtonStep1.click();
+   }
+    async selectStep2IndependenceOption(){
+    await this.independeceOptionStep2.click();
+   }
+    async selectStep2SafetyOption(){
+    await this.safetyOptionStep2.click();
+   }
+    async selectStep2TherapyOption(){
+    await this.therapyOptionStep2.click();
+   }
+    async selectStep2OtherOption(){
+    await this.otherOptionsStep2.click();
+   }
+   async selectStep3OwnedOption(){
+    await this.ownedOptionStep3.click();
+   }
+    async selectStep3RentalOption(){
+    await this.rentalOptionStep3.click();
+   }
+    async selectStep3MobileOption(){
+    await this.mobileOoptionStep3.click();
+   }
+   async clickNextButtonStep3(){
+    await this.nextButtonStep3.click();
    }
 
  }
--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -2,16 +2,65 @@ import { test, expect } from '@playwright/test';
 import * as testData from '../testData/testData.json';
 import { MainPage } from '../pages/mainPage';
 
-test('Verify that the main page is loaded', async ({ page }) => {
+test('Verify that the user can navigate to the main page', async ({ page }) => {
   const mainPage = new MainPage(page);
   await mainPage.goToMainPage();
   await expect(page).toHaveTitle(testData.validDataSet.mainPageTitle);
 });
 
-test('Ask for a cost quote scenario - Valid e2e flow with form steps verification', async ({ page }) => {
+test('Verify that the user can complete the cost quote flow while selecting all the options', async ({ page }) => {
   const mainPage = new MainPage(page);
   await mainPage.goToMainPage();
   await mainPage.enterZipCode(testData.validDataSet.zipNumberValid);
   await mainPage.clickSubmitButtonStep1();
- // at this point, should add step no verification
+  await mainPage.selectStep2IndependenceOption();
+  await mainPage.selectStep2SafetyOption();
+  await mainPage.selectStep2TherapyOption();
+  await mainPage.selectStep2OtherOption();
+  await mainPage.nextButtonStep2.click();
+  await mainPage.selectStep3OwnedOption();
+  await mainPage.selectStep3RentalOption();
+  await mainPage.selectStep3MobileOption();
+  await mainPage.clickNextButtonStep3();
+  await mainPage.enterNameStep4(testData.validDataSet.name);
+  await mainPage.enterEmailStep4(testData.validDataSet.email);
+  await mainPage.goToEstimateStep4.click();
+  await mainPage.enterPhoneNumberStep5(testData.validDataSet.phoneNumber);
+  await mainPage.submitYourRequestButtonStep5.click();
+  await expect(page).toHaveTitle(testData.validDataSet.thankYouPageTitle);
+});
+
+test('Verify that the user can complete the cost quote flow with only required options selection', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.goToMainPage();
+  await mainPage.enterZipCode(testData.validDataSet.zipNumberValid);
+  await mainPage.clickSubmitButtonStep1();
+  await mainPage.selectStep2IndependenceOption();
+  await mainPage.nextButtonStep2.click();
+  await mainPage.selectStep3RentalOption();
+  await mainPage.clickNextButtonStep3();
+  await mainPage.enterNameStep4(testData.validDataSet.name);
+  await mainPage.enterEmailStep4(testData.validDataSet.email);
+  await mainPage.goToEstimateStep4.click();
+  await mainPage.enterPhoneNumberStep5(testData.validDataSet.phoneNumber);
+  await mainPage.submitYourRequestButtonStep5.click();
+  await expect(page).toHaveTitle(testData.validDataSet.thankYouPageTitle);
+});
+
+test('Verify that while the user is navigating through the cost quote flow the step counter is updated accordingly', async ({ page }) => {
+  const mainPage = new MainPage(page);
+  await mainPage.goToMainPage();
+  await mainPage.enterZipCode(testData.validDataSet.zipNumberValid);
+  await mainPage.clickSubmitButtonStep1();
+  await mainPage.verifyFormCurrentStepNumber('2 of 5');
+  await mainPage.selectStep2IndependenceOption();
+  await mainPage.nextButtonStep2.click();
+  await mainPage.selectStep3RentalOption();
+  await mainPage.clickNextButtonStep3();
+  await mainPage.enterNameStep4(testData.validDataSet.name);
+  await mainPage.enterEmailStep4(testData.validDataSet.email);
+  await mainPage.goToEstimateStep4.click();
+  await mainPage.enterPhoneNumberStep5(testData.validDataSet.phoneNumber);
+  await mainPage.submitYourRequestButtonStep5.click();
+  await expect(page).toHaveTitle(testData.validDataSet.thankYouPageTitle);
 });
